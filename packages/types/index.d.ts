ff3c7af9453cd07b391cac9b2ab9a19724003566
zheng-yang-liu <thezhengyang@qq.com>
2024-07-09 17:23:07 +0800

    优化组件类型标注

--- packages/types/index.d.ts
+++ packages/types/index.d.ts
@@ -1,2 +1,6 @@
 import * as types from '/tools';
 import * as pageAnimation from '/pageAnimation';
+import * as xyButton from '../lib/xy-button/xy-button-type.ts';
+import * as xyClassicPage from '../lib/xy-classic-page/xy-classic-page.type.ts';
+import * as xyEffectPreview from '../lib/xy-effect-preview/effect.type.ts'
+import * as xyMenu from '../lib/xy-menu/xy-menu.type.ts';
